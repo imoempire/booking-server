cd2748fbc34b7fe7642a526e58feb61f259a2573
imoempire <mrimoempire@gmail.com>
2022-10-23 10:23:27 +0000

    modify

--- Controllers/customer.js
+++ Controllers/customer.js
@@ -1,6 +1,7 @@
 const { isValidObjectId } = require("mongoose");
 const SeatModel = require("../Model");
 const Customers = require("../Model/customer");
+const QueueModel = require("../Model/Queue");
 
 exports.bookTable = async (req, res) => {
   const { name, contact, customers } = req.body;
@@ -22,26 +23,29 @@ exports.bookTable = async (req, res) => {
       .json({ error: "Sorry no available tables join the qeuee with" });
 
   let tablesToBook = 0;
-  if (number <= 4) {
+  let customersnumbers = number;
+
+  
+
+  if (customersnumbers <= 4) {
     tablesToBook += 1;
-  } else if (number <= 8) {
+  } else if (customersnumbers <= 8) {
     tablesToBook += 2;
-  } else if (number <= 12) {
+  } else if (customersnumbers <= 12) {
     tablesToBook += 3;
-  } else if (number <= 16) {
+  } else if (customersnumbers <= 16) {
     tablesToBook += 4;
-  } else if (number <= 20) {
+  } else if (customersnumbers <= 20) {
     tablesToBook += 5;
-  } else if (number <= 24) {
+  } else if (customersnumbers <= 24) {
     tablesToBook += 6;
-  } else if (number <= 28) {
+  } else if (customersnumbers <= 28) {
     tablesToBook += 7;
-  } else if (number <= 32) {
+  } else if (customersnumbers <= 32) {
     tablesToBook += 8;
   } else {
     console.log("error");
   }
-
   let selectedTables = {};
   let bookedTable = [];
 
@@ -50,19 +54,26 @@ exports.bookTable = async (req, res) => {
     bookedTable.push(selectedTables);
   }
 
-  const tableLeft = tables.length;
-  let customersLeft = ''
-  if (tablesToBook > tableLeft) {
-    customersLeft = customers - (tableLeft*chairsPer)
-    return res.status(404).json({
-      error: `${tablesToBook} table(s) requied: But only ${tableLeft} available: head to ${selectedTables} and ${customersLeft} s required to wait till next available 
-      table, `,
-    });
-  }
+  let book = 0
 
-  const book = table - tablesToBook;
+   book = table - tablesToBook;
   const totalchairs = chairs.length;
   const chair = totalchairs - number;
+  
+  const tableLeft = tables.length;
+  let customersLeft = "";
+  if (tablesToBook > tableLeft) {
+    book = 0;
+    customersLeft = customers - tableLeft * chairsPer;
+    const queueNo = contact.slice(-2) + Math.floor(Math.random());
+    // console.log(queueNo);
+    // const newQueue = await QueueModel({name, queueNo })
+    return res.status(404).json({
+      error: `${tablesToBook} table(s) requied: But only ${tableLeft} available: head to ${selectedTables} and ${customersLeft} s required to wait till next available 
+      table, QUEUE No ${queueNo} `,
+    });
+  }
+  console.log(book);
 
   // Auto Label
   let tablesLeft = [];
@@ -76,6 +87,9 @@ exports.bookTable = async (req, res) => {
 
   // Update SeatModel
   tableChairs.table = book;
+  if(tablesToBook> tableLeft){
+    tableChairs.table = 0
+  }
   tableChairs.chairsPer = chairsPer;
   tableChairs.tables = tablesLeft;
   tableChairs.chairs = chairsLeft;
@@ -104,20 +118,17 @@ exports.bookTable = async (req, res) => {
 };
 
 exports.getCustomers = async (req, res) => {
-  
-  const items = await Customers.find()
+  const items = await Customers.find();
 
   const customersCount = await Customers.countDocuments();
 
   res.json({
-    customers: items.map((customer)=>(
-      {
-        id: customer.id,
-        name: customer.name,
-        contact: customer.contact,
-        customers: customer.customers,
-      }
-    )),
+    customers: items.map((customer) => ({
+      id: customer.id,
+      name: customer.name,
+      contact: customer.contact,
+      customers: customer.customers,
+    })),
     customersCount,
   });
 };
